d730f8b32e550348dc01879505ad55060803d397
Muhammad Zahiruddin <muhdzahir248@gmail.com>
2025-12-31 14:46:43 +0800

    improve analytics scroll style

--- resources/views/predictions/analytics.blade.php
+++ resources/views/predictions/analytics.blade.php
@@ -140,24 +140,6 @@
         background: #94a3b8;
     }
     
-    .cursor-main::-webkit-scrollbar {
-        width: 6px;
-    }
-    
-    .cursor-main::-webkit-scrollbar-track {
-        background: #f1f5f9;
-        border-radius: 3px;
-    }
-    
-    .cursor-main::-webkit-scrollbar-thumb {
-        background: #cbd5e1;
-        border-radius: 3px;
-    }
-    
-    .cursor-main::-webkit-scrollbar-thumb:hover {
-        background: #94a3b8;
-    }
-    
     @media (max-width: 1024px) {
         .cursor-layout {
             flex-direction: column;
